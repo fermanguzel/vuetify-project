ec7646573d9e0696a56de61299e2de958cd7cf53
fermanguzel <fermanguzel@gmail.com>
2023-12-16 00:09:06 +0300

    fer

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-import './assets/main.css'
+import "@mdi/font/css/materialdesignicons.css";
+import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "vuetify/styles";
+import App from "./App.vue";
+import router from "./router";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.use(vuetify);
+app.mount("#app");
---
+++ src/views/LoginView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const form = ref({
+  email: "",
+  password: "",
+  remember: false,
+});
+
+function submit() {
+  alert(JSON.stringify(form.value));
+}
+</script>
+
+<template>
+  <v-sheet width="300" class="mx-auto">
+    <v-form @submit.prevent="submit">
+      <v-text-field
+        variant="solo"
+        prepend-inner-icon="mdi-email"
+        v-model="form.email"
+        label="Email"
+      ></v-text-field>
+
+      <v-text-field
+        type="password"
+        variant="solo"
+        prepend-inner-icon="mdi-key"
+        v-model="form.password"
+        label="Password"
+      ></v-text-field>
+
+      <v-checkbox
+        v-model="form.remember"
+        label="Remember Me"
+        color="red-darken-1"
+      ></v-checkbox>
+      <v-btn type="submit" block class="mt-2" color="red-darken-1"
+        >Submit</v-btn
+      >
+    </v-form>
+  </v-sheet>
+</template>
